a3d1348e2c99c4c1022e03f436219c8bea8c8a73
kingaranjoroge <briankingara4@gmail.com>
2024-05-05 16:03:14 +0300

    modified home page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -4,11 +4,11 @@ const Home = () => {
   return (    
     <article className="HomeBackground">
       <section className='Home'>
-        <h1>Brian Njoroge</h1>
-        <h3>Web Developer</h3>
-        <h3>MERN Stack Developer</h3>
-        <h3>JavaScript Developer</h3>
-        <h3>(MongoDB, Express.js, React.js, Node.js)</h3>
+        <h1 className='text-blue-950'>Brian Njoroge</h1>
+        <h3 className='text-xl font-bold'>Full Stack Web Developer</h3>
+        <h3 className='text-lg font-medium'>Front End Developer (React.js, Next.js, TailwindCSS)</h3>
+        <h3 className='text-lg font-medium'>Back End Developer (Node.js, Express.js, Nest.js)</h3>
+        <h3 className='text-lg font-medium'>Database (MongoDB)</h3>
       </section>
     </article>    
   )

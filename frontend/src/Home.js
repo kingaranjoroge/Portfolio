2ed5d1c9e05494db75403a54cf11efd967549dfc
kingaranjoroge <briankingara4@gmail.com>
2024-09-05 18:29:49 +0300

    modify text size on small devices homepage

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -6,9 +6,9 @@ const Home = () => {
       <section className='Home'>
         <h1 className='text-blue-950'>Brian Njoroge</h1>
         <h3 className='text-xl font-bold'>Full Stack Web Developer</h3>
-        <h3 className='text-lg font-medium'>Front End Developer (React.js, Next.js, TailwindCSS)</h3>
-        <h3 className='text-lg font-medium'>Back End Developer (Node.js, Express.js, Nest.js)</h3>
-        <h3 className='text-lg font-medium'>Database (MongoDB)</h3>
+        <h3 className='text-sm md:text-lg font-medium'>Front End Developer (React.js, Next.js, TailwindCSS)</h3>
+        <h3 className='text-sm md:text-lg font-medium'>Back End Developer (Node.js, Express.js, Nest.js)</h3>
+        <h3 className='text-sm md:text-lg font-medium'>Database (MongoDB)</h3>
       </section>
     </article>    
   )

b815d760b713a94d8ed6b7b262d6d75c629a721d
kingaranjoroge <briankingara4@gmail.com>
2024-09-09 23:38:21 +0300

    modify styling

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -6,9 +6,11 @@ const Home = () => {
       <section className='Home'>
         <h1 className='text-blue-950'>Brian Njoroge</h1>
         <h3 className='text-xl font-bold'>Full Stack Web Developer</h3>
-        <h3 className='xs:text-xs sm:text-sm md:text-lg font-medium'>Front End Developer (React.js, Next.js, TailwindCSS)</h3>
-        <h3 className='xs:text-xs sm:text-sm md:text-lg font-medium'>Back End Developer (Node.js, Express.js, Nest.js)</h3>
-        <h3 className='xs:text-xs sm:text-sm md:text-lg font-medium'>Database (MongoDB)</h3>
+        <div className='flex flex-col items-center'>
+          <h3 className='xs:text-xs sm:text-sm md:text-lg font-medium whitespace-nowrap'>Front End Developer (React.js, Next.js, TailwindCSS)</h3>
+          <h3 className='xs:text-xs sm:text-sm md:text-lg font-medium whitespace-nowrap'>Back End Developer (Node.js, Express.js, Nest.js)</h3>
+          <h3 className='xs:text-xs sm:text-sm md:text-lg font-medium whitespace-nowrap'>Database (MongoDB)</h3>
+        </div>
       </section>
     </article>    
   )
